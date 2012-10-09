acbff1669679a3587d87a8dec0341b122ba01a1e
Benoît Chevallier <ben@mac-pro-de-ben.lan>
2012-10-09 18:09:33 +0200

    title

--- footer.php
+++ footer.php
@@ -13,6 +13,7 @@
 
 	<footer id="colophon" role="contentinfo">
 		<div id="site-generator">
+		<a name="contacts">les contacts</a><br />
 			<?php do_action( 'toolbox_credits' ); ?>
 			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'toolbox' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'toolbox' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'toolbox' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
