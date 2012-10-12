6176855ff434b75a93dc5cd4d6cd53737a996edc
Ben Chevallier <ben@so2design.ch>
2012-10-12 18:21:01 +0200

    menu ready

--- header.php
+++ header.php
@@ -44,6 +44,7 @@
 		echo ' | ' . sprintf( __( 'Page %s', 'toolbox' ), max( $paged, $page ) );
 
 	?></title>
+<link href='http://fonts.googleapis.com/css?family=Lato:400,700,900,400italic,700italic' rel='stylesheet' type='text/css'>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
